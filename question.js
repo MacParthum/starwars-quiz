9d93b99583fb27f1d408f44ae2b29abb360c0ed3
Mac Parthum <macparthum@gmail.com>
2020-08-06 18:09:59 -0400

    working skeleton with timer and questions array

---
+++ question.js
@@ -0,0 +1,12 @@
+var questions = [
+    {
+        question: "Who is lukes father?",
+        answers: ["Darth Vador", "boba fet", "pad ma", "pizza the hut"],
+        correct: 0
+    },
+    {
+        question: "Who is the worst star wars charcter?",
+        answers: ["Darth Vador", "boba fet", "jar jar binks", "pizza the hut"],
+        correct: 2
+    }
+]
--- script.js
+++ script.js
@@ -1,27 +1,61 @@
 var startButton = document.getElementById("start")
-
-var secondsLeft =10
-
+var questionScreen = document.getElementById("gameField")
+var secondsLeft =30
+var answersEl = document.getElementById("answers")
 var timer
+var currentQuestion = 0
+function time(){
+    timer = setInterval(function(){
+        secondsLeft--
+        document.getElementById("timer").textContent = "Time: " + secondsLeft + " sec" 
+        if(secondsLeft === 0){
+            clearInterval(timer)
+            console.log('end game')
+        }
 
-function time (){
-    timer = 
+    },1000)
 }
 
+function showQuestion (){
+    var questionEl = document.getElementById("question")
+    answersEl.innerHTML =""
+    questionEl.textContent = questions[currentQuestion].question
+    for (let i = 0; i < questions[currentQuestion].answers.length; i++) {
+        var button = document.createElement("button")
+        button.textContent = questions[currentQuestion].answers[i]
+        button.setAttribute('value', i )
+        button.classList.add("answer","btn", "btn-primary")
+        answersEl.appendChild(button)
+    }
 
+}
 
 function init(){
     //start timer
-
+    time()
     //make welcom screen invisible
+    var startScreen = document.getElementById("startScreen")
+    startScreen.setAttribute("class", "d-none")
     //display first question
+    questionScreen.classList.remove("d-none")
+    showQuestion()
+}
+
+function checkAnswer(){
+    if (!event.target.classList.contains('answer')) return;
+    var userGuess = parseInt(event.target.value)
+    if(userGuess !== questions[currentQuestion].correct){
+        secondsLeft = secondsLeft - 15
+        console.log('you are wrong')
+    }
+    currentQuestion++
+    showQuestion()
+
 }
 
 
 
 
 
-
-
-
+answersEl.addEventListener("click", checkAnswer)
 startButton.addEventListener("click", init)
