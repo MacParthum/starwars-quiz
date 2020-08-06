ae0f59a1f9271e28aa3f5949139e66f9912ba0cc
Mac Parthum <macparthum@gmail.com>
2020-08-06 17:18:20 -0400

    started the basic html structure

---
+++ script.js
@@ -0,0 +1,27 @@
+var startButton = document.getElementById("start")
+
+var secondsLeft =10
+
+var timer
+
+function time (){
+    timer = 
+}
+
+
+
+function init(){
+    //start timer
+
+    //make welcom screen invisible
+    //display first question
+}
+
+
+
+
+
+
+
+
+startButton.addEventListener("click", init)
